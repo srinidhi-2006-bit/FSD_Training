2034e0776635f74f2a24cf0c00bdccab1597694b
srinidhi-2006-bit <egalasrinidhireddy7@gmail.com>
2026-06-15 23:27:17 +0530

    JS Programs

---
+++ JS_PROGRAMS/module/Calculator.js
@@ -0,0 +1,38 @@
+/*
+COMMONJS MODULES uses module.export={}
+
+function add(a,b)
+{
+    return a+b;
+}
+function diff(a,b)
+{
+    return a-b;
+}
+function mul(a,b)
+{
+    return a*b;
+}
+function div(a,b)
+{
+    return a/b;
+}
+module.exports=add
+
+*/
+export function add(a,b)
+{
+    return a+b;
+}
+function diff(a,b)
+{
+    return a-b;
+}
+function mul(a,b)
+{
+    return a*b;
+}
+function div(a,b)
+{
+    return a/b;
+}
---
+++ JS_PROGRAMS/module/utility.js
@@ -0,0 +1,5 @@
+import {add} from "./Calculator.js"
+var a=10
+var b=20
+
+console.log(add(a,b))
