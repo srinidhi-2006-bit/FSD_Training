dfc18233373a05f1e2abf847bf36e0755347fd01
EGALA SRINIDHI REDDY <egalasrinidhireddy7@gmail.com>
2026-06-18 18:34:25 +0530

    Add files via upload

---
+++ productdemo/src/main/java/com/example/productdemo/Product.java
@@ -0,0 +1,43 @@
+package com.example.productdemo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+@Entity
+@Table(name = "products")
+public class Product {
+    @Id
+    private int pid;
+    private String pname;
+    private double price;
+    public Product() {}
+    public Product(int pid,String pname,double price){
+        this.pid = pid;
+        this.pname = pname;
+        this.price = price;
+    }
+    public int getPid() {
+        return pid;
+    }
+    public void setPid(int pid) {
+        this.pid = pid;
+    }
+    public String getPname() {
+        return pname;
+    }
+    public void setPname(String pname) {
+        this.pname = pname;
+    }
+    public double getPrice() {
+        return price;
+    }
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    @Override
+    public String toString() {
+        return "Product [pid=" + pid +
+                ", pname=" + pname +
+                ", price=" + price + "]";
+    }
+}
