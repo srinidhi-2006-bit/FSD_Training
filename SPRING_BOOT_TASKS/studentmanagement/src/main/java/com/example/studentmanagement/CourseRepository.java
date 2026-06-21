d9fe0b4003a9e049795c116dd0824e237f01701e
EGALA SRINIDHI REDDY <egalasrinidhireddy7@gmail.com>
2026-06-21 23:11:11 +0530

    Delete SPRING_BOOT_TASKS/studentmanagement directory

--- SPRING_BOOT_TASKS/studentmanagement/src/main/java/com/example/studentmanagement/CourseRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.studentmanagement;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-public interface CourseRepository
-        extends JpaRepository<Course,Integer> {
-
-}
--- SPRING_BOOT_TASKS/studentmanagement/src/main/java/com/example/studentmanagement/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.studentmanagement;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-public interface StudentRepository
-        extends JpaRepository<Student,Integer> {
-
-}
